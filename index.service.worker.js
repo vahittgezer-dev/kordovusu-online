630d73258fe15a3ce34cfbf1632a641343cb5437
mrtoxa42 <betebet16@hotmail.com>
2026-08-09 13:21:48 +0300

    Arkadasla mac oda akisini duzelt

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786263737|16645638';
+const CACHE_VERSION = '1786270688|69361839';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
