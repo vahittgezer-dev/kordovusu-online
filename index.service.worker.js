342f65d684cea9b7c755e45492ee2af89d6e3366
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 01:31:30 +0300

    Refresh expired sessions before loading friend rooms

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786811634|13829724';
+const CACHE_VERSION = '1786833080|27006447';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
