5b5379e70c9c924abc4cfc3a20b6cb21a1908228
mrtoxa42 <betebet16@hotmail.com>
2026-08-15 19:35:03 +0300

    Add fixed ten-room friend match lobby

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786808668|22245066';
+const CACHE_VERSION = '1786811634|13829724';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
