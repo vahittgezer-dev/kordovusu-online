3b496a4d3f68437390cdd6e7664ca54648cbb471
mrtoxa42 <betebet16@hotmail.com>
2026-08-15 15:59:52 +0300

    Update browser build and disable unverified purchases

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786303926|377331194';
+const CACHE_VERSION = '1786796480|15029959';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
