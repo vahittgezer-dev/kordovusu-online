a72ae1407e7669cbfd8a2367c7e1bac713b272c1
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 10:56:03 +0300

    Deploy updated friend lobby web build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786833080|27006447';
+const CACHE_VERSION = '1786866908|16459703';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
