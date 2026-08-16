76efbd3dfb71b20282c5781851f0ca4cf68c9ad5
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 15:22:06 +0300

    Align matchmaking text to right of shield and add stylish circular cancel button

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786882681|10456800';
+const CACHE_VERSION = '1786882905|10603521';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
