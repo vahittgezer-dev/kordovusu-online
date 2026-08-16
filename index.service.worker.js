0b6b8e7403f3a16d84f85a888ac5348e741843ef
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 17:25:12 +0300

    Streamline deck editor layout: merge title and squad counter into single top banner, enlarge cards with 10px spacing, make star and X buttons mini badges, adjust action buttons

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786889951|12771011';
+const CACHE_VERSION = '1786890302|7101964';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
