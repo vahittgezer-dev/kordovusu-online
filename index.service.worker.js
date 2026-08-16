dc6cd85e4e6c3e94f76b8278750b030f485ae4fd
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 17:36:53 +0300

    Fix card picker header stacking bug and replace card badges with sleek glowing green neon border

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786890843|17151397';
+const CACHE_VERSION = '1786891006|6555768';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
