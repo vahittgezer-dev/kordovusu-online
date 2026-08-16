0744cb8b6d06889d21829684f240623e42feb3a5
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 14:19:39 +0300

    Implement 25-35s ranked matchmaking, fallback bot and win-streak Zafer Yolu progression

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786877789|16446243';
+const CACHE_VERSION = '1786879163|9872151';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
