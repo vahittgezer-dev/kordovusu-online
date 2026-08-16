4a2688b70378bbe3bf103724ab31765bbd1198a6
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 19:00:00 +0300

    Fix cloud save persistence across logins and web fetch networking

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786891006|6555768';
+const CACHE_VERSION = "1785954338654";
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
