7f7a289ad919e7e937d4d850573f3ad30d1da44f
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 15:31:36 +0300

    Fix cloud sync button font readability and contrast in account details panel

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786883260|9940583';
+const CACHE_VERSION = '1786883481|9891697';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
