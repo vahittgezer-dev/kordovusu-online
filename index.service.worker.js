9e37591d6cfaf5fb970b701e710808432ccf4d1b
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 15:47:01 +0300

    Add Vault tab to Shop and smooth slide transition animations for pack navigation

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786884170|7844732';
+const CACHE_VERSION = '1786884413|7770569';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
