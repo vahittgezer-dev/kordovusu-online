959903bb6fb0f0a70534378350eb7a019da3d1f7
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 12:36:54 +0300

    Complete browser friend match handshake

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786871812|6560564';
+const CACHE_VERSION = '1786872972|6528377';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
