4c9f427545471c60dee170c2b3a5fec608c6af52
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 16:39:24 +0300

    Perfect Matchmaking HUD: transparent vector prestige shield, clean procedural obsidian-gold panel, and balanced cancel button

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786887459|7579944';
+const CACHE_VERSION = '1786887552|13419562';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
