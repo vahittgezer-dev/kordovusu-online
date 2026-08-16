a95af58c5741cbe0fdacaa2c085f40be8a8d6cfc
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 16:37:50 +0300

    Polish Ready Check modal layout, progress energy line, and themed buttons to perfectly fit frame asset

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786887320|8341034';
+const CACHE_VERSION = '1786887459|7579944';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
