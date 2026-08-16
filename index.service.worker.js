32b5b2c289e0b0e26b6ddd238726f042b11ca3b5
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 17:08:21 +0300

    Compact 7-card selected deck row with vertical stacked JOKER and CIKAR buttons for zero overflow

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786889135|9775116';
+const CACHE_VERSION = '1786889292|8105036';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
