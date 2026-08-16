0320d7d803ae50c49e035644cffc659c5c5662d7
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 15:18:23 +0300

    Polish matchmaking ribbon HUD layout, padding and integrated ruby cancel button

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786882307|22610417';
+const CACHE_VERSION = '1786882681|10456800';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
