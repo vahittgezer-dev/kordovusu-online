e6f1b626483b88dde033ae30e01ad896ae415215
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 14:24:43 +0300

    Ensure non-blocking matchmaking search and 25-35s continuous timer

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786879163|9872151';
+const CACHE_VERSION = '1786879470|9790056';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
