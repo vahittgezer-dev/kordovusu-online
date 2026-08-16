0466adf6b8b46a0bc35213fd020b8af168b70c5f
mrtoxa42 <betebet16@hotmail.com>
2026-08-16 11:22:52 +0300

    Fix browser friend lobby server connection

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786866908|16459703';
+const CACHE_VERSION = '1786868513|7519498';
 /** @type {string} */
 const CACHE_PREFIX = 'Kor Dovusu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
